664cdf10cd529694d578ae628ca6c100eaaccb5b
Semucas <semucas27@gmail.com>
2026-03-11 14:43:31 -0500

    chore: configurar esqueleto inicial Spring Boot

---
+++ chepa-api/src/main/java/com/chepamotos/chepa_api/ChepaApiApplication.java
@@ -0,0 +1,13 @@
+package com.chepamotos.chepa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChepaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChepaApiApplication.class, args);
+	}
+
+}
