45b71e02e6b40bac141f25602c84cd757d2b53ce
Abel Garcia <abel22561@gmail.com>
2026-03-18 21:29:43 -0500

    fix(api): evitar posible sobreescritura de variable

--- chepa-api/src/main/java/com/chepamotos/model/Invoice.java
+++ chepa-api/src/main/java/com/chepamotos/model/Invoice.java
@@ -52,9 +52,8 @@ public class Invoice {
 
     @PrePersist
     protected void onCreate() {
-
-        this.createdAt = LocalDateTime.now();
-
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
     }
-
 }
